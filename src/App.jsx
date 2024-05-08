d2b0e78c3d25b52ae881b824297e43748ea08792
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-05-08 23:44:17 +0530

    UI

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen bg-black">
+        <div className="h[-90%]" flex>
+          <Sidebar/>
+        </div>
+        <Player/>
+      </div>
+    </>
+  );
+}
+
+export default App;
